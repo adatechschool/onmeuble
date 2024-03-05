eaf80d4d0c029c0afdbef9aed61873336a4b7124
Arteinsana7 <147487242+Arteinsana7@users.noreply.github.com>
2024-03-05 11:52:12 +0100

    Création fichier route
    Création de routes user et route produits.

---
+++ .history/Routes/userRoute_20240305115004.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const users = require('../mocks/users.json'); 
+
+
+
+/*
+
+router.get('/users', (req, res) => {
+    res.json(users);
+  });
+
+  module.exports = router;   Example de utilisation de route*/ 
