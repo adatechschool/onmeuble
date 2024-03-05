45c82d81047f39ec4327c8f24d341c7496b85bf8
“Aurélien <aureliencartier@gmail.com>
2024-03-05 11:33:15 +0100

    Création du .gitignore, création du serveur, installation dépendance Express, suppression fichier UI front

---
+++ userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const users = require('../mocks/users.json'); 
+
+
+router.get('/users', (req, res) => {
+    res.json(users);
+  });
+
+  module.exports = router;
