e98aad275c17fd7db93db71ebd7ec9e6df6c2c70
Ryo Rafael <ryorafael18@gmail.com>
2024-03-12 16:02:06 +0100

    css et listarticle page ajouter modifier

--- front/src/pages/AdminPage.js
+++ front/src/pages/AdminPage.js
@@ -1,61 +1,74 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import "./AdminPage.css";
 
 function AdminPage() {
   const [product, setProduct] = useState({
-    name: '',
-    description: '',
-    price: '',
-    image: '',
-    color: '',
-    size: '',
-  })
+    name: "",
+    description: "",
+    price: "",
+    image: null,
+    color: "",
+    size: "",
+  });
 
   const handleInputChange = (event) => {
     setProduct({
       ...product,
       [event.target.name]: event.target.value,
-    })
-  }
+    });
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setProduct({
+      ...product,
+      image: file,
+    });
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     // send a request to the server to update the product
-  }
+    console.log(product)
+  };
 
   return (
-    <div className='admin'>
-      <h1>Admin Page</h1>
-      <p>Welcome to the administrator page!</p>
-      <form onSubmit={handleSubmit}>
+    <div className="admin">
+      <h1 className="adminpage">Admin Page</h1>
+      <p className="paraAdmin">Welcome to the administrator page!</p>
+      <form className="adminForm" onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
-        <input
+        <input 
+          className="product"
           type="text"
           name="name"
           value={product.name}
           onChange={handleInputChange}
         />
-        <label htmlFor="description">Description:</label>
+        <label className="productImg" htmlFor="image">Image:</label>
+        <input
+          type="file"
+          accept="image/*"
+          name="image"
+          onChange={handleImageChange}
+        />
+        {product.image && (
+          <img src={URL.createObjectURL(product.image)} alt="Product" />
+        )}
+        <label className="desc" htmlFor="description">Description:</label>
         <textarea
           name="description"
           value={product.description}
           onChange={handleInputChange}
         />
-        <label htmlFor="price">Price:</label>
+        <label className="price" htmlFor="price">Price:</label>
         <input
           type="number"
           name="price"
           value={product.price}
           onChange={handleInputChange}
         />
-        <label htmlFor="image">Image:</label>
-        <input
-          type="text"
-          name="image"
-          value={product.image}
-          onChange={handleInputChange}
-        />
-
-        <label htmlFor="image">color:</label>
+        <label className="productColor" htmlFor="image">Color:</label>
         <input
           type="text"
           name="color"
@@ -63,7 +76,7 @@ function AdminPage() {
           onChange={handleInputChange}
         />
 
-<label htmlFor="price">size:</label>
+        <label htmlFor="price">Size:</label>
         <input
           type="number"
           name="size"
@@ -71,7 +84,7 @@ function AdminPage() {
           onChange={handleInputChange}
         />
 
-        <label htmlFor="material">Material:</label>
+        <label className="material" htmlFor="material">Material:</label>
         <input
           type="text"
           name="material"
@@ -79,10 +92,10 @@ function AdminPage() {
           onChange={handleInputChange}
         />
 
-        <button type="submit">Update Product</button>
+        <button className="submitBtn" type="submit">Update Product</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default AdminPage
+export default AdminPage;
