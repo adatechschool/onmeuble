88d53bb25b48de2e16edc9a52be96288aff2a5d0
AlexShadow3 <alexandrebobis91@gmail.com>
2024-03-14 11:05:25 +0100

    Update Profile.js

--- front/src/pages/Profile.js
+++ front/src/pages/Profile.js
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-  const handleLogin = (e) => {
-    // e.preventDefault();
-    // Ici, vous pouvez ajouter votre logique d'identification avec le backend
-    // Une fois l'identification réussie, naviguez vers la page Dashboard
-    // navigate("/productsAdmin");
-    if (email === "laurelinefleuri@gmail.com" && password === "1234") {
-      console.log("Connexion réussie");
-      navigate("/productsAdmin");
-    } else {
-      console.log("Email ou mot de passe incorrect");
-      navigate("/profile");
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3000/login2", {
+        email,
+        password,
+      });
+      console.log("Response data:", response.data);
+      if (response.data.message === "Connexion réussie") {
+        navigate("/productsAdmin");
+      } else {
+        console.error("Erreur lors de la connexion:", response.data);
+        setError("Identifiants invalides.");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la connexion:", error);
+      setError("Une erreur est survenue lors de la connexion.");
     }
   };
+
+  if(error) {
+    console.log("Error:", error);
+  }
 
   return (
     <form className="login-form" onSubmit={handleLogin}>
@@ -43,6 +56,7 @@ function LoginForm() {
         />
       </div>
       <button type="submit">Se connecter</button>
+      <p id="error">Erreur</p>
     </form>
   );
 }
